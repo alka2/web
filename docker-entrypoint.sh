1f5bfd16f65dfacda1bddc70f42ddd0466ae7117
alka2 <s.alikarimi@gmail.com>
2020-06-15 16:46:09 -0400

    updated entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ "$ENVIRONMENT" == "DEV" ]
 then
-    cp /app/config/dev.js /app/src/App.js
+    cp /code/config/dev.js /code/src/App.js
 elif [ "$ENVIRONMENT" == "PROD" ]
 then
-    cp /app/config/prod.js /app/src/App.js
+    cp /code/config/prod.js /code/src/App.js
 fi
 
 exec "$@"
